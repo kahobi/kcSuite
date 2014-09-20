e3d34a61345246096f70c004e2500f0402ca8e75
Kahobi <kahobi@gmail.com>
2014-09-20 04:17:15 -0700

    Subroutine, floor, regular block and with conduit.

---
+++ kcAPI001.lua
@@ -0,0 +1,36 @@
+dofile("QCVar.lua")
+os.loadAPI("qcAPI.lua")
+
+function xfloorB (x)
+	qcAPI.xBlockForward(x , SmokedQ)							--Colum 2, Left Smoked, Send
+	qcAPI.mvNextR()
+
+	qcAPI.xBlockForward(x , NetherQ)							--Colum 3, Left Nether, Return
+	qcAPI.mvNextL()
+
+	qcAPI.xBlockForward(x , NetherQ)							--Colum 4, Center Nether, Send 
+	qcAPI.mvNextR()
+
+	qcAPI.xBlockForward(x , NetherQ)							--Colum 5, Right Nether, Return
+	qc.ColumL()
+
+	qcAPI.xBlockForward(x , NetherQ)							--Colum 6, Right Smoked, Send
+end
+
+function xfloorC (x)
+	qcAPI.xBlockForward(x , SmokedQ)							--Colum 2 - Left Smoked, Send
+	qcAPI.mvNextR()
+
+	qcAPI.xBlockForward(x , NetherQ)							--Colum 3, Left Nether, Return
+	qcAPI.mvNextL()
+
+	qcAPI.xConduitForward(x , NetherF)						--Colum 4, Centre Nether, Send
+	qcAPI.mvNextR()
+
+	qcAPI.xConduitForward(1 , NetherF)						--Colum 5, Right Nether, Return
+	qcAPI.xBlockForawrd((x - 1) , NetherQ) 
+	qc.ColumL()
+
+		qcAPI.xBlockForawrd((x - 1) , SmokedQ)				--Colum 6, Right Smoked, Send
+		qcAPI.xConduitForward(1 , SmokedF) 
+end
